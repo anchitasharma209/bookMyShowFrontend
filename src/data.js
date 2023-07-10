ef9ffd6e074df63123bd16d2714958df36443ba3
Anchita Sharma <123532000+anchitasharma209@users.noreply.github.com>
2023-07-10 17:25:35 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,6 @@
+
+export let moviesList = [ 'The Flash' , 'Neeyat', 'Satyaprem Ki Katha', '72 Hoorian','Elemental','Ajmer 92','Oppenheimer','Mission Impossible','Past Lives'];
+export let slots = ['10:00 AM', '01:00 PM', '03:00 PM',"05:00","08:00 PM", "11:30 PM"];
+export let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
+
+
